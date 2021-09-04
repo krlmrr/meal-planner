b331f3ed86caf94871210a6cd8afc081218f07f6
Karl Murray <kmurray@foxworldtravel.com>
2021-09-03 19:16:50 -0500

    Moved Token Methods to TokenController

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class TokenController extends Controller
+{
+    public function create(Request $request) 
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+
+    public function destroy(Request $request) 
+    {
+        $user = User::where('email', $request->email)->first();
+        $user->tokens()->where('id', $request->tokenId)->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,38 +3,17 @@
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TokenController;
 
 Auth::routes();
 
-Route::post('/token', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
+Route::post('/token', [TokenController::class,'create']);
 
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->device_name)->plainTextToken;
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::middleware('auth:sanctum')->post('/token/revoke', function (Request $request) {
-    $user = User::where('email', $request->email)->first();
-    $user->tokens()->where('id', $request->tokenId)->delete();
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/token/revoke', [TokenController::class, 'destroy']);
 });
 
 Route::get('/logout', function() {
