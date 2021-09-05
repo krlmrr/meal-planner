2ce639c6a8dcd38039c5169e532ab416b5f70861
Karl Murray <kmurray@foxworldtravel.com>
2021-09-05 15:26:04 -0500

    Got user test written

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase, WithoutMiddleware;
+
+    /**
+     * A test to see if a user can see their own info.
+     *
+     * @return void
+     */
+    public function test_user_can_see_their_info()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->json('GET', '/api/user');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+           'name' => $user['name'],
+           'email' => $user['email']
+        ]);
+    }
+}
